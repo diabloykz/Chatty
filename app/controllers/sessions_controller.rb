58179e5fc32af2c076809081d047ce078b16fc91
Thierry Poirine <tpoirine@gmail.com>
2019-07-01 13:25:28 -1000

    create and destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # tout est bon
+      session[:user_id] = user.id
+      flash[:success] = "Vous êtes bien connecté , #{user.username}"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Vos identifiants de connexion sont incorrects"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Vous êtes bien déconnecté"
+    redirect_to login_path
+  end
 end
