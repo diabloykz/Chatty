9db90c8e85d78e931277a524054b4c659dd547dc
Thierry Poirine <tpoirine@gmail.com>
2019-07-01 12:04:02 -1000

    Modification formulaire login et ajout méthodes d'authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if sessions[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "Vous devez être connecté pour effectuer cette action"
+      redirect_to login_path
+    end
+  end
 end
